a9ed79535ecb5b0a258660c5e74e0ad8e6b8e6d0
Fabricio-Guima <fsgkof@gmail.com>
2025-03-30 11:41:53 -0300

    add CategoryDTO

--- backend/src/main/java/com/fsg/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/fsg/dscatalog/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package com.fsg.dscatalog.resources;
 
+import com.fsg.dscatalog.dto.CategoryDTO;
 import com.fsg.dscatalog.entities.Category;
 import com.fsg.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +21,8 @@ public class CategoryResource {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
-        List<Category> categories = categoryService.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll(){
+        List<CategoryDTO> categories = categoryService.findAll();
 
         return ResponseEntity.ok().body(categories);
     }
