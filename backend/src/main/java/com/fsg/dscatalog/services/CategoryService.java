7830f58a18ac9a6a3723573867dde0aba7a2ed74
Fabricio-Guima <fsgkof@gmail.com>
2025-03-30 13:35:15 -0300

    Find category by id service

--- backend/src/main/java/com/fsg/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/fsg/dscatalog/services/CategoryService.java
@@ -3,12 +3,14 @@ package com.fsg.dscatalog.services;
 import com.fsg.dscatalog.dto.CategoryDTO;
 import com.fsg.dscatalog.entities.Category;
 import com.fsg.dscatalog.repositories.CategoryRepository;
+import com.fsg.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,5 +24,12 @@ public class CategoryService {
         List<Category> list = categoryRepository.findAll();
 
          return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
     }
 }
