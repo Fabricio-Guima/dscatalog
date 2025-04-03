c35da2535948904c7e229e3287ddf3240bb94186
Fabricio-Guima <fsgkof@gmail.com>
2025-04-03 11:30:55 -0300

    Delete category

--- backend/src/main/java/com/fsg/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/fsg/dscatalog/services/CategoryService.java
@@ -3,10 +3,14 @@ package com.fsg.dscatalog.services;
 import com.fsg.dscatalog.dto.CategoryDTO;
 import com.fsg.dscatalog.entities.Category;
 import com.fsg.dscatalog.repositories.CategoryRepository;
+import com.fsg.dscatalog.services.exceptions.DatabaseException;
 import com.fsg.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -50,6 +54,20 @@ public class CategoryService {
             return new CategoryDTO(entity);
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if(!categoryRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+
+        try {
+            categoryRepository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
         }
     }
 }
